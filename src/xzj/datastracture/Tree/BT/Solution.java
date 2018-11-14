466a3a039594ed0ceb807beec2dfb08c65c5dccb
xuzju023 <651879800@qq.com>
2018-11-14 17:15:54 +0800

    pathSum

--- src/xzj/datastracture/Tree/BT/Solution.java
+++ src/xzj/datastracture/Tree/BT/Solution.java
@@ -98,6 +98,33 @@ public class Solution {
         levelHelper(res, root, 0);
         return res;
     }
+	
+//	Given the below binary tree and sum = 22,
+//
+//		      5
+//		     / \
+//		    4   8
+//		   /    /   \
+//		  11  13  4
+//		 /  \        \
+//		7    2       1
+// return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+	public boolean hasPathSum(TreeNode root, int sum) {
+
+		return pathSum(root, sum, 0);
+	}
+
+	public boolean pathSum(TreeNode root, int sum, int total) {
+		if(root==null) {
+			return false;
+		}
+		total=total+root.val;
+		if (root.left==null&&root.right==null) {
+			return total==sum;
+		}
+		return pathSum(root.left, sum, total) || pathSum(root.right, sum, total);
+	}
+
     
     public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
         if (root == null) return;
