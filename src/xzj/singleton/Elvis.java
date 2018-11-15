905534302ca8d95b262975b5006577762a595a54
xuzju023 <651879800@qq.com>
2018-11-15 10:11:26 +0800

    fds

--- src/xzj/singleton/Elvis.java
+++ src/xzj/singleton/Elvis.java
@@ -4,6 +4,7 @@ public class Elvis {
 	private static final Elvis INSTANCE= new Elvis();
 	private Elvis() {}
 	public static Elvis getInstance() {
+		
 		return INSTANCE;
 	}
 	public static void main(String[] args) {
