ed8d22333a09547cdd3b879caeb4cd82a7cb8047
xuzju023 <651879800@qq.com>
2019-01-18 11:26:44 +0800

    Search for a Range

--- src/xzj/arithmetic/binarySearch/Solution.java
+++ src/xzj/arithmetic/binarySearch/Solution.java
@@ -1,6 +1,10 @@
 package xzj.arithmetic.binarySearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Solution  extends GuessGame{
 //		Example 1:
@@ -282,10 +286,62 @@ public class Solution  extends GuessGame{
     	return nums[end];
     }
 	
-	
-	
+
+//    Search for a Range
+//    Go to Discuss
+//  Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+//
+//  Your algorithm's runtime complexity must be in the order of O(log n).
+//
+//  If the target is not found in the array, return [-1, -1].
+//
+//  Example 1:
+//
+//  Input: nums = [5,7,7,8,8,10], target = 8
+//  Output: [3,4]
+//  Example 2:
+//
+//  Input: nums = [5,7,7,8,8,10], target = 6
+//  Output: [-1,-1]
+    public static int[] searchRange(int[] nums, int target) {
+    	int begin=0;
+    	int end =nums.length-1;
+        while(end>=begin) {
+        	int mid=(begin+end)/2;
+        	if(nums[mid]==target) {
+        		int left=mid;
+        		int right=mid;
+        		while(++right<=end) {
+        			if(target==nums[right]) {
+        			}else {
+        				break;
+        			}
+        		}
+        		while(--left>=begin) {
+        			if(target==nums[left]) {
+        			}else {
+        				break;
+        			}
+        		}
+        		return new int[] {left+1,right-1};
+        	}
+        	if(nums[mid]>target) {
+        		end=mid-1;
+        	}
+        	if(nums[mid]<target) {
+        		begin=mid+1;
+        	}
+        }
+    	return new int[] {-1,-1};
+    }
+    
+    
 	public static void main(String[] args) {
-		System.out.println( findPeakElement(new int[] {1,2,3,1}));
+		int[] nums= {5,7,7,8,8,10};
+		int[] searchRange = searchRange(nums, 8);
+		for (int i : searchRange) {
+			System.out.println(i);
+		}
 	}
 	
 }
