74c0a46bff965c7e846680db165bb8969b3069b2
xuzju023 <38324922+xuzju023@users.noreply.github.com>
2020-05-31 23:17:10 +0800

    2020/05/31

--- src/arithmetic/TreeNode.java
+++ src/arithmetic/TreeNode.java
@@ -2,7 +2,7 @@ package arithmetic;
 
 public class TreeNode {
           int val;
-      TreeNode left;
-     TreeNode right;
-      TreeNode(int x) { val = x; }
+      public TreeNode left;
+     public TreeNode right;
+      public TreeNode(int x) { val = x; }
 }
--- src/arithmetic/recursion/Recursion.java
+++ src/arithmetic/recursion/Recursion.java
@@ -1,5 +1,7 @@
 package arithmetic.recursion;
 
+import arithmetic.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,6 +76,7 @@ public class Recursion {
         int memo[] = new int[n + 1];
         return climb_Stairs(0, n, memo);
     }
+
     public static int climb_Stairs(int i, int n, int[] memo) {
         if (i > n) {
             return 0;
@@ -89,24 +92,86 @@ public class Recursion {
     }
 
     /**
-     *
-     *   输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+     * 输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
      */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> result = new ArrayList<>();
-        if(listNode!=null){
-            deal(listNode,result);
+        if (listNode != null) {
+            deal(listNode, result);
         }
 
         return result;
     }
-    public void deal(ListNode cur, ArrayList<Integer> result){
-        if(cur.next!=null){
-            deal(cur.next,result);
+
+    public void deal(ListNode cur, ArrayList<Integer> result) {
+        if (cur.next != null) {
+            deal(cur.next, result);
         }
         result.add(cur.val);
     }
 
+
+    //    Unique Binary Search Trees II
+//            Solution
+//    Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+//
+//    Example:
+//
+//    Input: 3
+//    Output:
+//            [
+//            [1,null,3,2],
+//            [3,2,null,1],
+//            [3,1,null,null,2],
+//            [2,1,3],
+//            [1,null,2,null,3]
+//            ]
+//    Explanation:
+//    The above output corresponds to the 5 unique BST's shown below:
+//
+//            1         3     3      2      1
+//            \       /     /      / \      \
+//            3     2     1      1   3      2
+//            /     /       \                 \
+//            2     1         2                 3
+    public List<TreeNode> generateTrees(int n) {
+
+        return genTrees(1, n);
+    }
+
+    public List<TreeNode> genTrees(int start, int end) {
+
+        List<TreeNode> list = new ArrayList<TreeNode>();
+
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+
+        if (start == end) {
+            list.add(new TreeNode(start));
+            return list;
+        }
+
+        List<TreeNode> left, right;
+        for (int i = start; i <= end; i++) {
+
+            left = genTrees(start, i - 1);
+            right = genTrees(i + 1, end);
+
+            for (TreeNode lnode : left) {
+                for (TreeNode rnode : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lnode;
+                    root.right = rnode;
+                    list.add(root);
+                }
+            }
+
+        }
+
+        return list;
+    }
 
 
     public static void main(String[] args) {
