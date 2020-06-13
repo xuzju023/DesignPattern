37efd67db4343143f34ee2a61eff2f0ddf2039a8
xzj <xu.zhijun@navercorp.com>
2020-06-13 15:16:40 +0800

    3

--- src/problem/leetcode/medium/LongestSubString.java
+++ src/problem/leetcode/medium/LongestSubString.java
@@ -29,11 +29,6 @@ public class LongestSubString {
 //    Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
     public static int lengthOfLongestSubstring(String s) {
 
-
-
-
-
-
         if (s.length()==0) return 0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int max=0;
